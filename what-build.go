f4bd9d43386726953b11c496e33caf8bf0c753d1
Sergey Kruk <sergey.kruk@gmail.com>
2019-02-03 15:26:05 +0100

    Refactor and implement configuration from AWS SSM
      * Move request building and build search to separate functions
      * Define SSM config format
      * Fix error of number conversion

--- what-build.go
+++ what-build.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
 	"github.com/joho/godotenv"
 )
 
@@ -15,66 +20,146 @@ func init() {
 	godotenv.Load()
 }
 
-type buildParameters struct {
-	DeployToQA string `json:"DEPLOY_TO_QA"`
-	QAEnv      string `json:"QA_ENV"`
-}
-type buildResponse struct {
-	BuildNum        int             `json:"build_num"`
-	Branch          string          `json:"branch"`
-	VcsRevision     string          `json:"vcs_revision"`
-	Subject         string          `json:"subject"`
-	Why             string          `json:"why"`
-	DontBuild       string          `json:"dont_build"`
-	StopTime        string          `json:"stop_time"`
-	BuildTimeMillis int             `json:"build_time_millis"`
-	Status          string          `json:"status"`
-	BuildParameters buildParameters `json:"build_parameters"`
+// Settings is a settings object for the crawler
+type Settings struct {
+	PerPage   int `json:"per_page"`
+	MaxOffset int `json:"max_offset"`
 }
 
-const perPage int = 10
+// Project contains info to fetch builds from CircleCI
+type Project struct {
+	Name  string `json:"name"`
+	URL   string `json:"url"`
+	Token string `json:"token"`
+}
 
-func main() {
+// Build contains search conditions and identification
+type Build struct {
+	Name            string                 `json:"name"`
+	BuildParameters map[string]interface{} `json:"build_parameters"`
+}
+
+// Config contains projects and builds along with settings for the crawler
+type Config struct {
+	Settings Settings  `json:"settings"`
+	Projects []Project `json:"projects"`
+	Builds   []Build   `json:"builds"`
+}
+
+// CIBuildResponse is a JSON extraits for build entity on circleci
+type CIBuildResponse struct {
+	BuildNum        int                    `json:"build_num"`
+	Branch          string                 `json:"branch"`
+	VcsRevision     string                 `json:"vcs_revision"`
+	Subject         string                 `json:"subject"`
+	Why             string                 `json:"why"`
+	DontBuild       string                 `json:"dont_build"`
+	StopTime        string                 `json:"stop_time"`
+	BuildTimeMillis int                    `json:"build_time_millis"`
+	Status          string                 `json:"status"`
+	BuildParameters map[string]interface{} `json:"build_parameters"`
+}
+
+func buildRequest(url string, token string, limit int, offset int) (req *http.Request, err error) {
+	req, err = http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	q := req.URL.Query()
+	q.Add("circle-token", token)
+	q.Add("limit", strconv.Itoa(limit))
+	q.Add("offset", strconv.Itoa(offset))
+	q.Add("filter", "completed")
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("User-Agent", "lonelyelk-what-build-bot")
+	req.URL.RawQuery = q.Encode()
+	return
+}
+func findCIBuild(projName string, buildName string, config *Config) (ciBuild *CIBuildResponse, err error) {
 	client := &http.Client{Timeout: 10 * time.Second}
-	for offset := 0; offset < 100; offset = offset + 10 {
-		req, err := http.NewRequest("GET", os.Getenv("PROJECT_URL"), nil)
-		if err != nil {
-			log.Println(err)
-			os.Exit(1)
-		}
-
-		q := req.URL.Query()
-		q.Add("circle-token", os.Getenv("CIRCLECI_TOKEN"))
-		q.Add("limit", string(perPage))
-		q.Add("offset", string(offset))
-		q.Add("filter", "completed")
-		req.Header.Add("Accept", "application/json")
-		req.Header.Add("User-Agent", "lonelyelk-what-build-bot")
-		req.URL.RawQuery = q.Encode()
-		res, err := client.Do(req)
-		if err != nil {
-			log.Println(err)
-			os.Exit(1)
-		}
-		defer res.Body.Close()
-		builds := make([]buildResponse, 0)
-		err = json.NewDecoder(res.Body).Decode(&builds)
-		if err != nil {
-			log.Println(err)
-			os.Exit(1)
-		}
-
-		for _, build := range builds {
-			if build.Why == "api" && build.BuildParameters.DeployToQA == "true" && build.Status == "success" {
-				time, err := time.Parse(time.RFC3339, build.StopTime)
-				if err != nil {
-					log.Println(err)
-				}
-				fmt.Printf("Deployed to %s at %v\nCommit: %s\nRevision: %s\n", build.BuildParameters.QAEnv, time, build.Subject, build.VcsRevision)
-				os.Exit(0)
-			}
+	var buildConfig *Build
+	var projectConfig *Project
+	for _, p := range config.Projects {
+		if p.Name == projName {
+			projectConfig = &p
+			break
 		}
 	}
-	fmt.Println("Deploy to QA not found")
-	os.Exit(-1)
+	for _, b := range config.Builds {
+		if b.Name == buildName {
+			buildConfig = &b
+			break
+		}
+	}
+	if projectConfig == nil {
+		return nil, errors.New("Project config not found")
+	}
+	if buildConfig == nil {
+		return nil, errors.New("Build config not found")
+	}
+	for offset := 0; offset < config.Settings.MaxOffset; offset = offset + config.Settings.PerPage {
+		req, err := buildRequest(projectConfig.URL, projectConfig.Token, config.Settings.PerPage, offset)
+		if err != nil {
+			return nil, err
+		}
+
+		res, err := client.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		defer res.Body.Close()
+		ciBuilds := make([]CIBuildResponse, 0)
+
+		if err = json.NewDecoder(res.Body).Decode(&ciBuilds); err != nil {
+			return nil, err
+		}
+
+		for _, cib := range ciBuilds {
+			if cib.BuildParameters == nil {
+				continue
+			}
+			match := true
+			for key, value := range buildConfig.BuildParameters {
+				if cib.BuildParameters[key] != value {
+					match = false
+					break
+				}
+			}
+			if match {
+				return &cib, nil
+			}
+		}
+		if len(ciBuilds) < config.Settings.PerPage {
+			break
+		}
+	}
+	return nil, errors.New("Build not found")
+}
+
+func main() {
+	parameterInput := ssm.GetParameterInput{Name: aws.String(os.Getenv("AWS_SSM_NAME_CONFIG"))}
+	ssmc := ssm.New(session.Must(session.NewSession(&aws.Config{Region: aws.String(os.Getenv("AWS_REGION"))})))
+	param, err := ssmc.GetParameter(&parameterInput)
+	if err != nil {
+		fmt.Println(err)
+	}
+	var config Config
+	if err = json.Unmarshal([]byte(*param.Parameter.Value), &config); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	ciBuild, err := findCIBuild("console", "qa3", &config)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	time, err := time.Parse(time.RFC3339, ciBuild.StopTime)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("Deployed to %s at %v\nCommit: %s\nRevision: %s\n", "qa3", time, ciBuild.Subject, ciBuild.VcsRevision)
+	os.Exit(0)
 }
